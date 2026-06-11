16bd0817967d7c0a9f7bc74ffe8a87d8084c5a7f
zwz-work <1576274385@qq.com>
2026-06-11 19:41:16 +0800

    signaling

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import {createApp} from 'vue'
-import {createPinia} from 'pinia'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
@@ -9,17 +9,17 @@ import router from './router'
 import '@/assets/styles/styles.scss'
 
 // 开发环境启用 vconsole
-// if (import.meta.env.DEV) {
-const VConsole =  import('vconsole')
-new VConsole.default()
-// }
+if (import.meta.env.DEV || 1) {
+  const VConsole = await import('vconsole')
+  new VConsole.default()
+}
 
 const app = createApp(App)
 const pinia = createPinia()
 
 // 注册所有图标
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-    app.component(key, component)
+  app.component(key, component)
 }
 
 app.use(pinia)
